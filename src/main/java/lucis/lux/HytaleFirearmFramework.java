b89ea67994d8bbb0d48d66376c10abc5b86b85e0
Tenebris Lux <tenebrislux.code@gmail.com>
2026-01-17 10:37:51 +0100

    feat!: make project compatible with Hytale
    Step 1: remove the old code and add new File structure
    Step 1.5: Remove template data
    
    # Conflicts:
    #	.gitignore
    #	pom.xml
    #	src/main/resources/manifest.json

---
+++ src/main/java/lucis/lux/HytaleFirearmFramework.java
@@ -0,0 +1,28 @@
+package lucis.lux;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class HytaleFirearmFramework extends JavaPlugin {
+
+    public HytaleFirearmFramework(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        // TODO: register components
+    }
+
+    @Override
+    protected void start() {
+        super.start();
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+    }
+}
